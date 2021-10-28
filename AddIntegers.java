1431fbcf370e8ca8ddc0d9cbc9fac901deffd7d5
Dalton <dheising@umich.edu>
2021-10-28 17:09:15 -0400

    Edit Second Integer

--- AddIntegers.java
+++ AddIntegers.java
@@ -3,7 +3,7 @@ public class AddIntegers {
     public static void main(String[] args) {
 
         int first = 15;
-        int second = 25;
+        int second = 55;
         int third = 35;
 
         System.out.println("Enter three numbers: " + first + " " + second + " " + third);
